22c7c0388716ce0708cd089f9fe2bf3d00a650d8
AndrewJBateman <gomezbateman@yahoo.com>
2021-07-25 13:25:01 +0200

    feat: crypto app completed

--- App.js
+++ App.js
@@ -8,16 +8,19 @@ import {
 	TextInput,
 } from "react-native";
 
-import CoinItem from "./components/CoinItem"
+import CoinItem from "./components/CoinItem";
 
 const App = () => {
 	const [coins, setCoins] = useState([]);
+	const [refreshing, setRefreshing] = useState(false);
+	const [search, setSearch] = useState("");
 
 	const loadData = async () => {
 		const res = await fetch(
 			"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 		);
 		const data = await res.json();
+    console.log('data: ', data);
 		setCoins(data);
 	};
 
@@ -27,10 +30,32 @@ const App = () => {
 
 	return (
 		<View style={styles.container}>
+			<StatusBar backgroundColor="#0e0275" />
+
+			<View style={styles.header}>
+				<Text style={styles.title}>CryptoCurrencies</Text>
+				<TextInput
+					style={styles.searchInput}
+					placeholder="Search Cryptos"
+					placeholderTextColor="#858585"
+					onChangeText={(text) => text && setSearch(text)}
+				/>
+			</View>
+
 			<FlatList
-				data={coins}
-				renderItem={({ item }) => {
-					return <CoinItem coin={item}/>;
+				style={styles.list}
+				data={coins.filter(
+					(coin) =>
+						coin.name.toLowerCase().includes(search.toLocaleLowerCase()) ||
+						coin.symbol.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+				)}
+				showsVerticalScrollIndicator={false}
+				renderItem={({ item }) => <CoinItem coin={item} />}
+				refreshing={refreshing}
+				onRefresh={async () => {
+					setRefreshing(true);
+					await loadData();
+					setRefreshing(false);
 				}}
 			/>
 		</View>
@@ -40,8 +65,29 @@ const App = () => {
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: "#141414",
-		alignItems: "center",
 		flex: 1,
+		alignItems: "center",
+	},
+	header: {
+		flexDirection: "row",
+		width: "90%",
+		justifyContent: "space-between",
+		marginBottom: 10,
+	},
+	title: {
+		fontSize: 20,
+		color: "#fff",
+		marginTop: 10,
+	},
+	list: {
+		width: "90%",
+	},
+	searchInput: {
+		color: "#fff",
+		borderBottomColor: "#c8cbfa",
+		borderBottomWidth: 1,
+		width: "40%",
+		textAlign: "left",
 	},
 });
 
